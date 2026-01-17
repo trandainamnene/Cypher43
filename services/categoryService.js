d60e0ff6c01afcf80f70c74379949623be7c2a57
trandainamnene <dainamne123@gmail.com>
2026-01-17 19:48:31 +0700

    fix: sync news articles when category name is updated

--- services/categoryService.js
+++ services/categoryService.js
@@ -19,7 +19,23 @@ module.exports = {
     getAllCategories,
     createCategory,
     updateCategory: async (id, data) => {
-        return await Category.findByIdAndUpdate(id, data, { new: true });
+        // Get the old category to check if name changed
+        const oldCategory = await Category.findById(id);
+        if (!oldCategory) return null;
+
+        // Update the category
+        const updatedCategory = await Category.findByIdAndUpdate(id, data, { new: true });
+
+        // If the category name changed and it's a news category, update all related news
+        if (oldCategory.name !== data.name && oldCategory.type === 'news') {
+            const News = require('../models/News');
+            await News.updateMany(
+                { category: oldCategory.name },
+                { category: data.name }
+            );
+        }
+
+        return updatedCategory;
     },
     deleteCategory: async (id) => {
         return await Category.findByIdAndDelete(id);
