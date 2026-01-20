041d20291334b5b1084737cf277972613e5fa8d1
trandainamnene <dainamne123@gmail.com>
2026-01-20 15:26:43 +0700

    fix: update CORS allowed origins to cipher43.com

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.use(cors({
     const allowedOrigins = [
       'http://localhost:5173',
       'http://localhost:3000',
-      'https://cipher43lab.com',
-      'https://www.cipher43lab.com',
+      'https://cipher43.com',
+      'https://www.cipher43.com',
       process.env.FRONTEND_URL,
       process.env.VERCEL_URL
     ].filter(Boolean);
